cbc6671968b0a590467fdeead786248e01b25fd8
ModischFabrications <modisch.fabrications@gmail.com>
2021-12-28 17:22:28 +0100

    extract wifi into seperate functions

--- src/shared/network/WiFiLoginManager.h
+++ src/shared/network/WiFiLoginManager.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <WiFiManager.h> // captive portal for wifi
-// TODO include guard for ESP8266/ESP32
-#include <ESPmDNS.h>
+#include <WiFiManager.h> // https://github.com/tzapu/WiFiManager
+// TODO include guard for ESP8266/ESP32?
+//#include <ESPmDNS.h>
 
 #include "shared/SerialWrapper.h"
 
@@ -14,27 +14,28 @@ WiFiManager wifiManager;
 
 // gets called when WiFiManager enters configuration mode
 void configModeCallback(WiFiManager* myWiFiManager) {
-    println(F("Entered config mode"));
+    println(F("Entered config mode, connect manually to: "));
     // auto generated SSID might be unknown
     printlnRaw(myWiFiManager->getConfigPortalSSID());
     printlnRaw(WiFi.softAPIP().toString());
 }
 
-void setup(const char* name) {
+void setupWiFi(const char* name) {
     println(F("setting up wifi (with captive portal)"));
 
     // DEBUGGING:
     // wifiManager.resetSettings();
 
     wifiManager.setAPCallback(configModeCallback);
+    //wifiManager.setConnectTimeout(5);
+    wifiManager.setMinimumSignalQuality(10);
     wifiManager.setAPStaticIPConfig(IPAddress(192, 168, 1, 1), IPAddress(192, 168, 1, 1),
                                     IPAddress(255, 255, 255, 0));
-    // wifiManager.setConfigPortalTimeout(60);
 
-    // LAN name if successful
+    // LAN name if successfully connected
     WiFi.hostname(name);
 
-    // hotspot name if not
+    // try connecting, show captive portal on fail
     if (!wifiManager.autoConnect(name)) {
         logError(F("failed to connect and hit timeout"));
         // reset and try again after waking up
@@ -44,11 +45,19 @@ void setup(const char* name) {
 
     // if you get here you have connected to the WiFi
     println(F("connected to the local network"));
-
+}
+/*
+void setupMDNS(const char* name){
     // TODO register MDNS
     if (!MDNS.begin(name)) { // Start the mDNS responder
         logWarning(F("Error setting up mDNS responder!"));
     }
+}*/
+
+void setup(const char* name) {
+    setupWiFi(name);
+    // setupMDNS(name);
+    // TODO test connection?
 }
 
 } // namespace WiFiLoginManager
