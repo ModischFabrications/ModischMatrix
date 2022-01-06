d039e68be5efbb885b3774175ca3435312cfca71
ModischFabrications <modisch.fabrications@gmail.com>
2022-01-06 22:31:31 +0100

    split mode decoding and config, actually update internal mode

--- src/controller.h
+++ src/controller.h
@@ -1,28 +1,62 @@
 #pragma once
 
 #include "display.h"
-#include "shared/serialWrapper.h"
+#include "modes/clock.h"
 #include "shared/network/WiFiLoginManager.h"
 #include "shared/persistence/persistenceManager.h"
-#include "modes/clock.h"
+#include "shared/serialWrapper.h"
 #include <Arduino.h>
 
 namespace Controller {
-enum Mode : uint8_t { OFF = 0, STATIC, CLOCK, WEATHER };
+enum Mode : uint8_t { OFF = 0, STATIC, LOGIN, CLOCK, WEATHER, SIZE };
 
+void turnOff();
+void setMode(uint8_t new_mode);
+void printText(const String& msg);
+void showLogin();
+
+namespace {
 Mode mode;
 uint32_t timeout = 0;
 
-void setMode(uint8_t new_mode);
-void printText(const String& msg);
+void _toMode(Mode new_mode) {
+    if (new_mode == mode)
+        return;
+    print(F("New Mode: "));
+    switch (new_mode) {
+    case OFF:
+        println(F("OFF"));
+        turnOff();
+        break;
+    case STATIC:
+        println(F("STATIC"));
+        break;
+    case LOGIN:
+        println(F("LOGIN"));
+        showLogin();
+        break;
+    case CLOCK:
+        println(F("CLOCK"));
+        Display::clear();
+        break;
+    case WEATHER:
+        println(F("WEATHER"));
+        Display::clear();
+        break;
+    default:
+        println(F("UNKNOWN, discarding..."));
+        _toMode(OFF);
+        return;
+    }
+    mode = new_mode;
+}
+} // namespace
 
 void hideAfter(uint16_t milliseconds) { timeout = millis() + milliseconds; }
 
-void turnOff() { Display::clear(); }
-
-void setVisibility(bool on) {
-    if (!on)
-        turnOff();
+void turnOff() {
+    Display::clear();
+    setMode(OFF);
 }
 
 void setBrightness(uint8_t brightness) {
@@ -30,12 +64,6 @@ void setBrightness(uint8_t brightness) {
     config.brightness = brightness;
     PersistenceManager::set(config);
     // display updated from config callback
-}
-
-void updateConfig() {
-    Config::Configuration config = PersistenceManager::get();
-    Display::setBrightness(config.brightness);
-    // TODO switch to state
 }
 
 void printText(const String& msg) {
@@ -48,27 +76,34 @@ void showLogin() {
     msg.reserve(80);
     msg += F("\n");
     msg += WiFi.localIP().toString();
-    printText(msg);
+    Display::clear();
+    Display::printText(msg);
+    setMode(LOGIN);
 }
 
 void setMode(uint8_t new_mode) {
-    print(F("New Mode: "));
-    printlnRaw(new_mode);
-    switch (new_mode) {
-    case OFF:
-        turnOff();
-        break;
-    default:
-        break;
+    if (new_mode >= SIZE) {
+        print(F("New Mode "));
+        printRaw(new_mode);
+        println(F(" is out of bounds!"));
+        return;
     }
+
     Config::Configuration config = PersistenceManager::get();
     config.mode = new_mode;
     PersistenceManager::set(config);
+
+    // mode updated from config callback
+}
+
+void updateConfig() {
+    println(F("C: Loading config..."));
+    Config::Configuration config = PersistenceManager::get();
+    Display::setBrightness(config.brightness);
+    _toMode((Mode)config.mode);
 }
 
 void setup() {
-    setVisibility(true);
-    // TODO
     PersistenceManager::registerListener(updateConfig);
 
     Modes_Clock::setup();
@@ -76,14 +111,13 @@ void setup() {
 
 void loop() {
     uint32_t now = millis();
-    if (now > timeout && timeout != 0) {
+    if (timeout != 0 && now > timeout) {
+        println(F("C: Timeout"));
         timeout = 0;
         setMode(OFF);
     }
 
     switch (mode) {
-    case OFF:
-    case STATIC:
     default:
         break;
     case CLOCK:
@@ -91,7 +125,8 @@ void loop() {
         break;
     case WEATHER:
         // TODO pass to weather extension
-        printText("Too Cold");
+        printText("Too Cold!\n (TODO)");
+        delay(500);
         break;
     }
 }
