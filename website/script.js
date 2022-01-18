6036e4852e6884e577e9f76b25b4ec7361d71163
ModischFabrications <modisch.fabrications@gmail.com>
2022-01-18 22:42:11 +0100

    add print, timeout and title

--- website/script.js
+++ website/script.js
@@ -1,5 +1,42 @@
 "use strict";
 
-function onBodyLoad(){
-    // IM CALLED!
-  }
+function init() {
+}
+
+function onValueChange(element, targetVar) {
+    let newValue = element.value;
+    console.log(`"${targetVar}" changed to ${newValue}`);
+
+    switch (targetVar) {
+        case 'mode':
+            break;
+        case 'brightness':
+            brightnessOutput.value = newValue + "%";
+            break;
+        case 'timeout':
+            let x = new Date(newValue * 1000).toISOString().substr(11, 8);
+            timeoutOutput.value = x;
+        default:
+            break;
+    }
+
+}
+
+function sendUpdate() {
+    if (brightnessEnabled.value) {
+        // TODO append "?brightness=${newValue/100*255}"
+    }
+
+}
+
+let snackbarTimeout = null;
+function showSnackbar(text) {
+    let el = document.getElementById('snackbar');
+    el.innerHTML = text;
+    el.className = 'show';
+
+    clearTimeout(snackbarTimeout);
+    setTimeout(function () {
+        el.className = el.className.replace('show', '');
+    }, 5000)
+}
