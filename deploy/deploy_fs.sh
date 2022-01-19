a97b781a8db3d63fb3555132e178b07c7beaf6e8
ModischFabrications <modisch.fabrications@gmail.com>
2022-01-19 22:44:03 +0100

    typo

--- deploy/deploy_fs.sh
+++ deploy/deploy_fs.sh
@@ -7,7 +7,7 @@ echo "[MPC] --- Deploying to target device file system --- "
 
 command -v pio >/dev/null 2>&1 || { echo >&2 "pio can't be found. Add to PATH!"; exit 1; }
 
-echo "[MPC] prepare /data"
+echo "[MPC] clearing /data"
 rm -rf ./data/
 mkdir ./data/
 
