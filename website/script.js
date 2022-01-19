3f7157869b27575f02dc4a8e572ec7cfd153c013
ModischFabrications <modisch.fabrications@gmail.com>
2022-01-19 20:45:27 +0100

    use tabs for mode selection

--- website/script.js
+++ website/script.js
@@ -1,11 +1,33 @@
 "use strict";
 
 function init() {
+    openTab(document.getElementById("mode_0"));
 }
+
+function openTab(element) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(element.id + "_tab").style.display = "block";
+    element.className  += " active";
+  }
 
 function onValueChange(element, targetVar) {
     let newValue = element.value;
-    console.log(`"${targetVar}" changed to ${newValue}`);
+    //console.log(`"${targetVar}" changed to ${newValue}`);
 
     switch (targetVar) {
         case 'mode':
